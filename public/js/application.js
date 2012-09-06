1d7243473b1502da69a0d206a61992e24baa7644
Devon St. Clair <devonstclair@gmail.com>
2012-09-06 11:07:38 -0700

    Social stream modals and basic click events

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,41 @@
 $(document).ready(function() {
 
   //Global vars
-  
+
+  $('#mainTabs a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+  });
+
+  $('.subNav a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+
+    if($(this).attr('href') != '#listings') {
+      $('.details-container').hide();
+    }
+    else {
+      $('.details-container').show();
+    }
+  })
+
+
+  // Application Modals
+
+  //Individual modal
+  $('.individualListing').click(function (e) {
+    e.preventDefault();
+    $('#receiptModal').modal('show')
+  })
+
+  $('.editSocialStream').live('click', showSocialStreams);
+
+  function showSocialStreams(e) {
+    e.preventDefault();
+    console.log('hi there!');
+
+    $('#socialListingModal').modal('show')
+  }
+
+
 });
