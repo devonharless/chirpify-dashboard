cd16c85726f1e256b551ff9b31b9a2dd48080f9a
Devon St. Clair <devonstclair@gmail.com>
2012-09-09 22:54:57 -0700

    Managing user flow and modal layouts

--- public/js/application.js
+++ public/js/application.js
@@ -36,35 +36,83 @@ $(document).ready(function() {
   $('.subNav a').click(function (e) {
     e.preventDefault();
     $(this).tab('show');
-
-    if($(this).attr('href') != '#listings') {
-      $('.details-container').hide();
-    }
-    else {
-      $('.details-container').show();
-    }
   })
 
 
   //--- Main Listing View --- 'index.erb' & partials/_receipt.erb' & 'partials/_listingFilters.erb'
 
-  //individual modal
+  // Show an individual listing after user selection of a receipt
   $('.individualListing').click(function (e) {
     e.preventDefault();
+
+    //show an individual listing & hide the main receipt/listing
     $('#receiptModal').modal('show');
+    
   })
+
+  // Individual Listing > Edit Listing
+
+  $('#editListingBtn').live('click', editIndividualListing);
+  $('.individualBC').live('click', cancelEditListing);
+
+
+  // Edit Listing > Cancel
+
+  $('#editListingCancel').live('click', cancelEditListing);
+  $('#currentSocialModal .close').live('click', closeSocialContent);
+  $('#currentSocialModal .closeModal').live('click', closeSocialContent);
+  $('#currentSocialModal .saveEdits').live('click', saveSocialContent);
 
 
   //--- New Listing View --- 'partials/_createListing.erb'
+  
   $('.editSocialStream').live('click', showSocialStreams);
   $('.pubSaveNewListing').live('click', pubSaveNewLisiting);
 
 
 
-
   /*------------------------------------------------------------------
-  [3. iew specific functions ]
+  [3. View-specific functions ]
   */
+
+
+  //--- Individual Listing View --- 'modals/individualListing.erb'
+
+  function editIndividualListing(e) {
+    e.preventDefault();
+
+    //User intends to edit their pre-existing listing after reviewing their receipt info
+    //View swaps to a view similars to 'partials/creatListing.erb', content in inputs, form, etc.
+    
+    $('#listing_ID').fadeOut();
+    $('#editListing').removeClass('hide');
+
+    $('.individualBC').removeClass('active').html('<li><a href="#">Individual listing title</a> <span class="divider">/</span></li>');
+    $('.modal-header .breadcrumb').append('<li class="editBC active">Edit listing</li>');
+  }
+
+  function saveSocialContent(e) {
+    //Save the user's edits to their pre-existing social content for their listing
+    $('#currentSocialModal').modal('hide');
+  }
+
+  function closeSocialContent(e) {
+    e.preventDefault();
+
+    //No changes are saved
+    $('#currentSocialModal').modal('hide');
+  }
+
+  //--- Edit Individual Listing View / Cancel --- 'modals/individualListing.erb'
+
+  function cancelEditListing(e) {
+    //Cancel editing an individual listing - return to the individual listing in passive viewing mode
+    $('#editListing').addClass('hide');
+    $('#listing_ID').fadeIn();
+
+    $('.individualBC').addClass('active').html('Individual listing title');
+    $('.editBC').remove();
+  }
 
   //--- New Listing View --- 'partials/_createListing.erb'
 
@@ -91,7 +139,14 @@ $(document).ready(function() {
   function showSocialStreams(e) {
     e.preventDefault();
 
-    $('#socialListingModal').modal('show')
+    console.log('e.target >> ', e.target.id)
+
+    if(e.target.id == 'editNewChirpify') {
+      $('#newSocialModal').modal('show');
+    }
+    else {
+      $('#currentSocialModal').modal('show');
+    }
   }
 
 });
